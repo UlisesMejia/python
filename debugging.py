38f731de48a72bde23e619b3d0a1ccd8d1065b44
Ulises Mejia Ortega <perverell@gmail.com>
2022-05-08 01:59:07 -0500

    Debugging, hay que hacer ejercicios del estilo, el depurador de vs code esta sencillo

---
+++ debugging.py
@@ -0,0 +1,11 @@
+def divisors(num):
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+    return divisors     
+
+def run():
+    num = int(input("Ingresa un numero "))
+    print(divisors(num))
+    print("Termino el programa ")
+
+if __name__ == "__main__":
+    run()
