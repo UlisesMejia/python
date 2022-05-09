02130dc3800cb9d31903dcea97a28c94a0962898
Ulises Mejia Ortega <perverell@gmail.com>
2022-05-09 13:23:05 -0500

    se agregan ultimas clases;Assert statements y manejo de archivos

---
+++ assert_statements.py
@@ -0,0 +1,11 @@
+def divisors(num):
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+    return divisors     
+
+def run():
+        num = input("Ingresa un numero ")
+        assert num.isnumeric(), "Hay que ingresar un numero "
+        print(divisors(int(num)))
+        print("Termino el programa ")
+if __name__ == "__main__":
+    run()
